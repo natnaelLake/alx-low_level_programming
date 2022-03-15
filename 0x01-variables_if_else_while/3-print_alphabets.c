b104a7a66c0b26b91a17eb236fe9f9f8b4089921
natnael <natnaelLake>
2022-03-15 13:49:37 +0300

    added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the numbers from 00 to 99
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+for (c = 'A'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
+
