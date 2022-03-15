6efd9f0b8eaf75f7cf4d6e9d3c743c4c9374daf6
natnael <natnaelLake>
2022-03-15 14:07:56 +0300

    added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the numbers since 0 to 9
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int  n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
