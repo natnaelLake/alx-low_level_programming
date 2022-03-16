6a467f68c02f05c1a738cdaaa850ad8ee63d1f95
natnael <natnaelLake>
2022-03-16 12:20:40 +0300

    addedd

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - a function that prints the alphabet, in lowercase
+* Return: Always 0 (Succes)
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
